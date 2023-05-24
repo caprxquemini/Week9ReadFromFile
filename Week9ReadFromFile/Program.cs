251d455e086c5994c5eb7f72bcd100b706278e59
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-24 13:06:19 +0300

    Add project files.

---
+++ Week9ReadFromFile/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string fileDirectoryPath = @"C:\data";
+string fileName = "chuck.txt";
+
+string fullFilePath = Path.Combine(fileDirectoryPath, fileName);
+
+if (File.Exists(fullFilePath))
+{
+    Console.WriteLine($"{fileName} exists in {fileDirectoryPath}.");
+    string[] ChuckJokes = File.ReadAllLines(fullFilePath);
+    Console.WriteLine($"There are {ChuckJokes.Length} jokes about Chuck Norris in the file.");
+
+
+    foreach (string joke in ChuckJokes)
+    {
+        Console.WriteLine(joke);
+    }
+}
+
+else
+{
+    Console.WriteLine($"{fileName} was not found.");
+}
